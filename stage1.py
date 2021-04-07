57322267a604435b4b203a4c4ba8f15f7d5934a5
StephenKennedy5 <60448727+StephenKennedy5@users.noreply.github.com>
2021-04-06 18:41:08 -0700

    Add files via upload

--- stage1.py
+++ stage1.py
@@ -4,13 +4,16 @@ import time
 import sys
 from datetime import datetime
 from pytz import timezone
+from plyer import notification
 
 '''Run at 10am to get start time of favorite team game for the night
 let person know if there team is not playing '''
 
 def start_time():
     PATH = 'C:\Program Files (x86)\chromedriver'
-    driver = webdriver.Chrome(PATH)
+    option = webdriver.ChromeOptions()
+    option.add_argument('headless')
+    driver = webdriver.Chrome(PATH,chrome_options=option)
     driver.get('https://www.nba.com/schedule')
     time.sleep(3)
     try:
@@ -36,46 +39,80 @@ def start_time():
         temp = '/html/body/div[1]/div[2]/div[3]/section/div/div[2]/div[1]/div[1]/div[2]/div[2]/div[' + str(i+1) + ']'
         game = driver.find_element_by_xpath(temp).text
         game_split = game.split('\n')
+        print(game_split[0])
+        print(i)
         if game_split[0] == 'FINAL':
             game_info = [game_split[z] for z in post_info]
         elif game_split[0] == 'FINAL/OT':
             pass
         elif game_split[0] == 'FINAL/OT2':
             pass
+        # elif game_split[0] == 'LIVE HALF':
+        #     for p in range(len(game_split)):
+        #         print(p)
+        #         print(game_split[p])
         else:
             game_info = [game_split[z] for z in live_info]
+            print(game_info)
         today_games.append(game_info)
+        print(i)
 
     driver.close()
 
     # favorite_team = input('Who is your favorite team: ').title()
-    favorite_team = 'Nets'
+    favorite_team = 'Clippers'
+    play_tonight = False
 
     for games in today_games:
         for game in games:
             if favorite_team in game:
                 tip_off = games[0]
                 favorite_team_game = games
+                play_tonight = True
 
-    split_time = tip_off.split(' ')
-    split_time.remove('ET')
+    if play_tonight == True:
+        split_time = tip_off.split(' ')
+        split_time.remove('ET')
+        new_time = ' '.join(split_time)
+        new_time_standard = datetime.strptime(new_time, '%I:%M %p')
+        eastern = timezone('US/Eastern')
+        east_time = eastern.localize(new_time_standard)
+        pacific = timezone('US/PACIFIC')
+        western_time = east_time.astimezone(pacific)
+        tip_time = western_time.strftime('%H:%M')
+        tip_time_pm = western_time.strftime('%I:%M %p')
+    else:
+        tip_time = None
+        favorite_team_game = None
 
-    new_time = ' '.join(split_time)
-    new_time_standard = datetime.strptime(new_time, '%I:%M %p')
-    eastern = timezone('US/Eastern')
-    east_time = eastern.localize(new_time_standard)
-    pacific = timezone('US/PACIFIC')
-    western_time = east_time.astimezone(pacific)
-    tip_time = western_time.strftime('%H:%M')
+    return today_games,favorite_team, tip_time, favorite_team_game, play_tonight,tip_time_pm
 
-    return today_games,favorite_team, tip_time, favorite_team_game
-
-def time_file(today_games,favorite_team, tip_time, favorite_team_game):
-     file1 = open('time.txt','w')
-     file1.write(favorite_team + '\n')
-     file1.write(tip_time + '\n')
-     file1.close()
+def time_file(favorite_team, tip_time, play_tonight):
+    if play_tonight == True:
+        file1 = open('C:\\Users\\steph\\notification\\time.txt','w')
+        file1.write(favorite_team + '\n')
+        file1.write(tip_time + '\n')
+        file1.close()
+    else:
+        file1 = open('time.txt', 'w')
+        file1.write('Your ' + favorite_team + ' has the night off.')
+        file1.close()
 
 
-today_games, favorite_team, tip_time, favorite_team_game = start_time()
-time_file(today_games,favorite_team, tip_time, favorite_team_game)
+today_games, favorite_team, tip_time, favorite_team_game, play_tonight, tip_time_pm = start_time()
+time_file(favorite_team, tip_time, play_tonight)
+
+if bool(favorite_team_game) == True:
+    notification.notify(
+        title='Game Day!',
+        message='The ' + favorite_team + ' plays at ' + tip_time_pm + ' tonight.',
+        app_icon='C:\\Users\\steph\\notification\\basketball.ico',  # e.g. 'C:\\icon_32x32.ico'
+        timeout=10,  # seconds
+    )
+else:
+    notification.notify(
+        title='Day Off',
+        message= 'The ' + favorite_team + ' has the night off.',
+        app_icon=None,  # e.g. 'C:\\icon_32x32.ico'
+        timeout=10,  # seconds
+    )
